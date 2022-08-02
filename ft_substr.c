8665b7821d8d80f7b8738e3fd1da38ca56470a4b
Sirapat Charukamnertkanok <sirapat1charu@gmail.com>
2022-08-03 04:13:54 +0800

    man

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: scharuka <scharuka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 03:14:57 by scharuka          #+#    #+#             */
-/*   Updated: 2022/08/03 04:05:03 by scharuka         ###   ########.fr       */
+/*   Updated: 2022/08/03 04:13:28 by scharuka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,18 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*ans;
 	size_t	i;
+	size_t	reallen;
 
 	i = 0;
-	ans = malloc(sizeof(char) * (len + 1));
+	reallen = len;
+	if (start >= ft_strlen(s))
+		reallen = 0;
+	else if (ft_strlen(s) - start < len)
+		reallen = ft_strlen(s) - start;
+	ans = malloc(sizeof(char) * (reallen + 1));
 	if (!ans)
 		return(NULL);
-	ans = ft_memcpy(ans, (s + start), len);
+	ans = ft_memcpy(ans, (s + start), reallen);
 	ans[len - 1] = '\0';
 	return(ans);
 }
