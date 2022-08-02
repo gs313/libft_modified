584abdba6326a90afea319e6ead1afc6a8beb2f2
Sirapat Charukamnertkanok <sirapat1charu@gmail.com>
2022-08-02 22:29:44 +0800

    null case

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: scharuka <scharuka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 22:14:38 by scharuka          #+#    #+#             */
-/*   Updated: 2022/08/02 22:27:13 by scharuka         ###   ########.fr       */
+/*   Updated: 2022/08/02 22:29:23 by scharuka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ size_t	ft_strlcpy(char *restrict dst, const char *restrict src, size_t dstsize)
 	size_t i;
 
 	i = 0;
-	while(i < dstsize)
+	while(i < dstsize && src[i])
 	{
 		((char *)dst)[i] = ((char *)src)[i];
 		i++;
