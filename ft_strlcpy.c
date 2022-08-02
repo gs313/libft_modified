61362249d5ab84f73d77ed2deb1976ddbb642524
Sirapat Charukamnertkanok <sirapat1charu@gmail.com>
2022-08-02 22:33:57 +0800

    null case

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: scharuka <scharuka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 22:14:38 by scharuka          #+#    #+#             */
-/*   Updated: 2022/08/02 22:32:03 by scharuka         ###   ########.fr       */
+/*   Updated: 2022/08/02 22:33:49 by scharuka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ size_t	ft_strlcpy(char *restrict dst, const char *restrict src, size_t dstsize)
 		((char *)dst)[i] = ((char *)src)[i];
 		i++;
 	}
-	dst[++i] = '\0';
+	i++;
+	dst[i] = '\0';
 	return(ft_strlen(src));
 }
