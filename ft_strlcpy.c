523d4ac29593396f00b2116535c1b323d19176d1
Sirapat Charukamnertkanok <sirapat1charu@gmail.com>
2022-08-02 22:56:36 +0800

    remove *restrict

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,13 +6,13 @@
 /*   By: scharuka <scharuka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 22:14:38 by scharuka          #+#    #+#             */
-/*   Updated: 2022/08/02 22:37:59 by scharuka         ###   ########.fr       */
+/*   Updated: 2022/08/02 22:56:17 by scharuka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *restrict dst, const char *restrict src, size_t dstsize)
+size_t	ft_strlcpy(char dst, const char src, size_t dstsize)
 {
 	size_t i;
 
