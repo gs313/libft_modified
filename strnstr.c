e5b484eaced86c92354fbe861f1beed866f1d6a8
Sirapat Charukamnertkanok <sirapat1charu@gmail.com>
2022-08-03 02:13:58 +0800

    NULL

--- strnstr.c
+++ strnstr.c
@@ -6,7 +6,7 @@
 /*   By: scharuka <scharuka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 01:20:53 by scharuka          #+#    #+#             */
-/*   Updated: 2022/08/03 02:11:47 by scharuka         ###   ########.fr       */
+/*   Updated: 2022/08/03 02:13:51 by scharuka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,5 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 			return ((char *)haystack + i - j + 1);
 		i++;
 	}
-	return (0);
+	return (NULL);
 }
