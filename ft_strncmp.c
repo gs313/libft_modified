0b2651ed2cf4c142828b55c57087e8f3ec82909d
Sirapat Charukamnertkanok <sirapat1charu@gmail.com>
2022-08-03 00:01:22 +0800

    ft_strncmp

---
+++ ft_strncmp.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scharuka <scharuka@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/02 23:53:22 by scharuka          #+#    #+#             */
+/*   Updated: 2022/08/03 00:00:38 by scharuka         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"libft.h"
+
+int		ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+	size_t len;
+	
+	i = 0;
+	len = ft_strlen(s1);
+	while(i < len)
+	{
+		if(s1[i] != s2[i])
+		{
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		}
+	}
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
