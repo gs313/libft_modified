4b0521c6b8160e56c884af38efe91b638312958e
Sirapat Charukamnertkanok <sirapat1charu@gmail.com>
2022-08-02 22:50:20 +0800

    first auto git

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: scharuka <scharuka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 22:14:38 by scharuka          #+#    #+#             */
-/*   Updated: 2022/08/02 22:33:49 by scharuka         ###   ########.fr       */
+/*   Updated: 2022/08/02 22:37:59 by scharuka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,10 @@ size_t	ft_strlcpy(char *restrict dst, const char *restrict src, size_t dstsize)
 	size_t i;
 
 	i = 0;
+	if(!dstsize)
+	{
+		return (ft_strlen(src));
+	}
 	while(i < (dstsize - 1) && src[i])
 	{
 		((char *)dst)[i] = ((char *)src)[i];
