e1bb0338b9596a3fa41191a2a510f82fd7f99bde
Sirapat Charukamnertkanok <sirapat1charu@gmail.com>
2022-08-03 01:14:18 +0800

    int

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: scharuka <scharuka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 01:01:49 by scharuka          #+#    #+#             */
-/*   Updated: 2022/08/03 01:12:30 by scharuka         ###   ########.fr       */
+/*   Updated: 2022/08/03 01:14:13 by scharuka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n)
 	while (i < n && (((unsigned char *)s1)[i] || ((unsigned char *)s2)[i]))
 	{
 		if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i])
-			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
+			return((int)(((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]));
 		i++;
 	}
 	return (0);
