be744b040335d36addafb8574e724799fa0f4e32
Sirapat Charukamnertkanok <sirapat1charu@gmail.com>
2022-08-03 03:12:02 +0800

    strdup

---
+++ ft_strdup.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scharuka <scharuka@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/03 03:01:37 by scharuka          #+#    #+#             */
+/*   Updated: 2022/08/03 03:11:48 by scharuka         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	char *str;
+	unsigned int i;
+
+	i = 0;
+	str = malloc(sizeof(char) * (ft_strlen(s1)+1));
+	if(!str)
+		return (NULL);
+	while(s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	return (str);
+}
