119947107f49a8a7136ef64907c4df016c2ff6f2
Sirapat Charukamnertkanok <sirapat1charu@gmail.com>
2022-08-02 23:25:03 +0800

    strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scharuka <scharuka@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/02 23:11:42 by scharuka          #+#    #+#             */
+/*   Updated: 2022/08/02 23:24:49 by scharuka         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"libft.h"
+
+size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
+{
+	size_t	i;
+	size_t	start;
+
+	i = ft_strlen(dst);
+	start = i;
+	while (i < (dstsize - 1) && src[i - start])
+	{
+		dst[i] = src[i - start];
+		i++;
+	}
+	while (i < dstsize)
+		dst[i] = '\0';
+	return (start + ft_strlen(src));
+}
