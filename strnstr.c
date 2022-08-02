f5d84796fa0c3c8731fb9028839616a330f75bb4
Sirapat Charukamnertkanok <sirapat1charu@gmail.com>
2022-08-03 01:46:21 +0800

    strnstr

--- strnstr.c
+++ strnstr.c
@@ -6,7 +6,7 @@
 /*   By: scharuka <scharuka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 01:20:53 by scharuka          #+#    #+#             */
-/*   Updated: 2022/08/03 01:37:56 by scharuka         ###   ########.fr       */
+/*   Updated: 2022/08/03 01:45:21 by scharuka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,14 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
 	size_t	j;
+	size_t	nlen;
 	
 	i = 0;
 	j = 0;
+	nlen = strlen(needle);
 	if (!needle[0])
 		return ((char *)haystack);
-	while (i < len && haystack[i])
+	while (i <= len - nlen && haystack[i])
 	{
 		if (haystack[i] == needle[j])
 			j++;
