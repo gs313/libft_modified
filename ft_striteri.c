5e48c2800f0221dd1032a2137e99e87d790afed6
Sirapat Charukamnertkanok <scharuka@z1t2c3.42bangkok.com>
2022-08-26 23:13:09 +0700

    fix makefile and a little striteri

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: scharuka <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/19 23:46:16 by scharuka          #+#    #+#             */
-/*   Updated: 2022/08/20 08:15:27 by scharuka         ###   ########.fr       */
+/*   Updated: 2022/08/25 00:11:15 by scharuka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 	size_t	l;
 	size_t	i;
 
-	if (!s)
+	if (!s || !f)
 		return ;
 	i = 0;
 	l = ft_strlen(s);
